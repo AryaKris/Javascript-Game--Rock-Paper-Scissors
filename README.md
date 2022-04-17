3f98c0e706ed5d8709f427a2364a372527242a18
Arya Krishna <dr.aryakris@outlook.com>
2022-04-16 22:48:48 -0700

    added script.js file and read me

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Javascript-Game--Rock-Paper-Scissors
 Created a web page that allows players to interact with the game. The various elements on the web page can be used to submit inputs or display outputs.
+
+The following functions are implemented in the course of this project 
+
+
+
+* startGame() - Starts the game and displays input prompts.
+* endGame(event) - Triggered by user input. Computes and displays the end of the game.
+* calcResult(move1,move2)- Called in endGame() to compute the result and return it as a string. The moves are a number between 0 - 2 corresponding to the index of the moveList. A victory is returned when move 1 wins over move 2.
+* randomMove() - A function that returns a random number between 0 - 2 . Used to generate the computer move in calcResult().
---
+++ script.js
@@ -0,0 +1,43 @@
+let winMsg = 'Victory';
+let loseMsg = 'Crushing Defeat';
+let tieMsg = 'Tie';
+let moveList = ['Rock', 'Paper', 'Scissors'];
+/**
+ * Computes result of the game. returns victory message if move 1 wins.
+ * @param  {Number}   move1  move 1
+ * @param  {Number}   move2  move 2
+ * @return {String}   result result of the game
+ */
+
+function calcResult(move1, move2) { }
+
+/**
+ * @return {Number}   random number between 0 and 2
+ */
+
+function randomMove() {
+    return Math.floor(Math.random() * 3);
+}
+
+/**
+ * Displays start state of game
+ */
+
+function startGame() { }
+
+/**
+ * Displays end state of game
+ * @param {Event} event event containing information of users input.
+ */
+
+function endGame(event) { }
+
+startGame();
+// Similar to how variables allow us to organize our data, functions allow us to organize our code.
+
+let startGame = (argument) => {
+    return;
+};
+
+
+
